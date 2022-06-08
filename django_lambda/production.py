120cc31e6ceea29399543935a7a8b96c56557632
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2022-06-08 15:45:43 -0300

    Adjustments and improvements to custom stages

--- django_lambda/production.py
+++ django_lambda/production.py
@@ -1,3 +1,17 @@
+# Fix ImproperlyConfigured: SQLite 3.9.0 or later is required (found 3.7.17)
+__import__("pysqlite3")
+import sys
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
+import environ
 from .settings import *
 
+env = environ.Env()
+env.read_env(str(BASE_DIR / ".env.production"))
+
 DEBUG = False
+
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
