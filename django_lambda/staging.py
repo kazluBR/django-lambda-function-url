9acb49012353341e6d178b72cbcbbc928a8ee0ed
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2022-06-08 22:24:00 -0300

    Uploading static files to s3 bucket

--- django_lambda/staging.py
+++ django_lambda/staging.py
@@ -15,3 +15,19 @@ DEBUG = False
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+
+ADMIN_URL = env("DJANGO_ADMIN_URL")
+
+INSTALLED_APPS += ["storages"]
+
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=86400",
+}
+AWS_LOCATION = "static"
+
+STATIC_ROOT = str(BASE_DIR / "staticfiles")
+STATICFILES_DIRS = [str(BASE_DIR / "static")]
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
