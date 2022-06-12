88be5192a3d9c8ab2813c9ab619ea4e2e9d160dd
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2022-06-12 11:11:15 -0300

    Setting log retention days

--- django_lambda/staging.py
+++ django_lambda/staging.py
@@ -20,8 +20,6 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = "static"
 
-STATIC_ROOT = str(BASE_DIR / "staticfiles")
-STATICFILES_DIRS = [str(BASE_DIR / "static")]
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
