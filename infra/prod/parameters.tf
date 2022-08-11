fa90dd79d9e7157a30ddcb831760aff2f9b6f504
Lucas Rollemberg <lucas.rollemberg@gmail.com>
2022-08-11 10:50:49 -0300

    Naming parameters per stage

--- infra/prod/parameters.tf
+++ infra/prod/parameters.tf
@@ -1,35 +1,35 @@
 resource "aws_ssm_parameter" "access_key" {
-  name  = "/s3-user/access-key"
+  name  = "/prod/s3-user/access-key"
   type  = "SecureString"
   value = aws_iam_access_key.s3_user_access_key.id
 }
 
 resource "aws_ssm_parameter" "access_secret" {
-  name  = "/s3-user/access-secret"
+  name  = "/prod/s3-user/access-secret"
   type  = "SecureString"
   value = aws_iam_access_key.s3_user_access_key.secret
 }
 
 resource "aws_ssm_parameter" "db_endpoint" {
-  name  = "/database/endpoint"
+  name  = "/prod/database/endpoint"
   type  = "SecureString"
   value = aws_db_instance.database.address
 }
 
 resource "aws_ssm_parameter" "db_name" {
-  name  = "/database/name"
+  name  = "/prod/database/name"
   type  = "SecureString"
   value = var.db_name
 }
 
 resource "aws_ssm_parameter" "db_user" {
-  name  = "/database/user"
+  name  = "/prod/database/user"
   type  = "SecureString"
   value = var.db_user
 }
 
 resource "aws_ssm_parameter" "db_password" {
-  name  = "/database/password"
+  name  = "/prod/database/password"
   type  = "SecureString"
   value = random_string.db_password.result
 }
